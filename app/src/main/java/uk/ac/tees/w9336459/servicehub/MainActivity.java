9591729a53131f0677fd18b36b3d376ac26a39b2
yashpatel <yashpatel61016@gmail.com>
2021-02-28 02:57:10 +0000

    Created Welcome Screens

--- app/src/main/java/uk/ac/tees/w9336459/servicehub/MainActivity.java
+++ app/src/main/java/uk/ac/tees/w9336459/servicehub/MainActivity.java
@@ -2,13 +2,27 @@ package uk.ac.tees.w9336459.servicehub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button getStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            getStarted = findViewById(R.id.getstarted);
+
+            getStarted.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, welcome.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
-}
---
+++ app/src/main/java/uk/ac/tees/w9336459/servicehub/welcome.java
@@ -0,0 +1,19 @@
+package uk.ac.tees.w9336459.servicehub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_welcome);
+
+}
+}
