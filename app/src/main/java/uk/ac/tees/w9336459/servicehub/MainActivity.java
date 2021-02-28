ffa029444241ae945f533989f7c9f7fcf774125c
yashpatel <yashpatel61016@gmail.com>
2021-02-28 14:29:39 +0000

    fixes to welcome screens

--- app/src/main/java/uk/ac/tees/w9336459/servicehub/MainActivity.java
+++ app/src/main/java/uk/ac/tees/w9336459/servicehub/MainActivity.java
@@ -4,25 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button getStarted;
+    TextView t1 , t2 , t3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
             getStarted = findViewById(R.id.getstarted);
+            t1 = findViewById(R.id.serviceHub);
+            t2 = findViewById(R.id.getAnything);
+            t3 = findViewById(R.id.sitting);
 
-            getStarted.setOnClickListener(new View.OnClickListener(){
-
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this, welcome.class);
-                    startActivity(intent);
-                }
+            getStarted.setOnClickListener((view)->{
+                Intent a = new Intent(MainActivity.this , welcome.class);
+                startActivity(a);
             });
         }
     }
--- app/src/main/java/uk/ac/tees/w9336459/servicehub/welcome.java
+++ app/src/main/java/uk/ac/tees/w9336459/servicehub/welcome.java
@@ -1,19 +1,27 @@
 package uk.ac.tees.w9336459.servicehub;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 public class welcome extends AppCompatActivity {
+
+    TextView t1 , t2;
+    Button B1, B2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_welcome);
+
+        t1 = findViewById(R.id.welcome);
+        t2 = findViewById(R.id.signInAs);
+
+        B1 = findViewById(R.id.User_bt);
+        B2 = findViewById(R.id.ServiceProvider_bt);
+
 
 }
 }
