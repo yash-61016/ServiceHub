0a3d18db715e3cb8650a58490c812cd04347e8f8
W9359412 <79636802+W9359412@users.noreply.github.com>
2021-03-02 11:46:09 +0000

    Added User Login Screens

--- app/src/main/java/uk/ac/tees/w9336459/servicehub/welcome.java
+++ app/src/main/java/uk/ac/tees/w9336459/servicehub/welcome.java
@@ -2,18 +2,27 @@ package uk.ac.tees.w9336459.servicehub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class welcome extends AppCompatActivity {
 
+    Button btUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_welcome);
+
+        btUser = findViewById(R.id.User);
+
+        btUser.setOnClickListener((v)->{
+            Intent a = new Intent(welcome.this , UserSignUp.class);
+            startActivity(a);
+
+        });
 
 }
 }
